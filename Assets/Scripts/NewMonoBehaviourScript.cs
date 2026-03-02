52f15a63d1c811e2a941019a6a346ab77a1f1c85
Rasmus-Jacobsen <musse.jacobsen09@gmail.com>
2026-03-02 11:54:31 +0100

    Commit #3

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Entity
     {
-        
-    }
+        public int PlayerHP = 20;
+       
+        public Button attack;
+        public bool playerTurn;
+        public void Start()
+        {
+            if (attack != null)
+            {
+                attack.onClick.AddListener(PlayerAttack);
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 }

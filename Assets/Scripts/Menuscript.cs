aeea8391fc9ae624599cafbb59f5eab8d8845361
Rasmus-Jacobsen <musse.jacobsen09@gmail.com>
2026-03-13 12:12:50 +0100

    ghyhgj

--- Assets/Scripts/Menuscript.cs
+++ Assets/Scripts/Menuscript.cs
@@ -14,6 +14,7 @@ public class Menuscript : MonoBehaviour
     }
     public void Retry() 
     {
-       SceneManager.LoadScene("MainMenu"); // laddar scenen MainMenu)
+       SceneManager.LoadScene("Startmenu"); 
+
     }
 }

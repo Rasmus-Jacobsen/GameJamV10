349f9c30ab456da99b9e88257e2772d890178b57
Nogard-Inmo <liam.nordlund09@gmail.com>
2026-03-25 21:14:27 +0100

    added IngameMenu and Map

--- Assets/Scripts/Menuscript.cs
+++ Assets/Scripts/Menuscript.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menuscript : MonoBehaviour
 {
+
+    [SerializeField] 
+        GameObject credits, ingameMenu, map;
+
+
     // RASMUS
 
     public void PlayGame()
@@ -14,7 +19,37 @@ public class Menuscript : MonoBehaviour
     }
     public void Retry() 
     {
-       SceneManager.LoadScene("Startmenu"); 
+       SceneManager.LoadScene(0); 
 
+    }
+    public void OpenCredits()
+    {
+        credits.SetActive(true);
+    }
+    public void CloseCredits()
+    {
+        credits.SetActive(false);
+    }
+
+    public void OpenMap()
+    {
+        map.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void CloseMap()
+    {
+        map.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void OpenIngameMenu()
+    {
+        ingameMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void CloseIngameMenu()
+    {
+        ingameMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 }
